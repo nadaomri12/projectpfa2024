7f90432b9c90542fd62e2ceb72ac804f4d24d1c2
EYA <benamoreya57@gmail.com>
2024-04-02 01:25:10 +0200

    hi

--- backend/src/main/java/pfaaa/backend/auth/AuthenticationController.java
+++ backend/src/main/java/pfaaa/backend/auth/AuthenticationController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "api/auth")
 public class AuthenticationController {
     @Autowired
