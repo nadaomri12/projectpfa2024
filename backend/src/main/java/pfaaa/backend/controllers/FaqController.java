67fb23d1617d3c078d9195e98655f8828e0ab698
EYA <benamoreya57@gmail.com>
2024-04-13 21:00:09 +0200

    hi

--- backend/src/main/java/pfaaa/backend/controllers/FaqController.java
+++ backend/src/main/java/pfaaa/backend/controllers/FaqController.java
@@ -18,11 +18,14 @@ public class FaqController {
     //1-postfaq
     @PostMapping("/faqs")
     public ResponseEntity<FAQ> CreateFaq(@RequestBody FAQ faq) {
+        System.out.println("hello from post faq");
         return ResponseEntity.ok(faqService.createFaq(faq));
     }
     //2-getfaq
     @GetMapping("/faqs")
     public ResponseEntity<List<FAQ>> getAllFaqs() {
+        System.out.println("hello from get faq");
+
         List<FAQ> AllUsers = faqService.getALLFaqs();
         return ResponseEntity.ok().body(AllUsers);
     }
